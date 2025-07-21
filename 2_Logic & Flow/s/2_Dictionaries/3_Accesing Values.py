23cc37cfa58be0c675bf17d8e20f42c5550cc697
dwanat <dominik.wanat@obramat.es>
2025-07-21 16:46:12 +0200

    2025-07-21

---
+++ 2_Logic & Flow/s/2_Dictionaries/3_Accesing Values.py
@@ -0,0 +1,5 @@
+def get_capital(country_capitals, country_name):
+    # Write code here
+    name = country_capitals[country_name]
+    return name
+    
---
+++ 2_Logic & Flow/s/2_Dictionaries/4_Modyfing Dictionaries.py
@@ -0,0 +1,5 @@
+def update_employee_info(employee_dict, key, value):
+    # Write code here
+    employee_dict[key] = value
+
+    return employee_dict
