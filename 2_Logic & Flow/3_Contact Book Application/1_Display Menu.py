cacb2cb4239a5eb7d4f526a3c2d6c3881e87bc25
dominikwanat <dominik.wanat@gmail.com>
2025-09-22 00:00:30 +0200

    2025-09-21

---
+++ 2_Logic & Flow/3_Contact Book Application/1_Display Menu.py
@@ -0,0 +1,30 @@
+# In this project, we will build a Contact Book application 
+# step by step by breaking it into small, manageable functions.
+# The first step is to create a display_menu function.
+
+# Create a function named display_menu that prints the main menu
+# options for the Contact Book.
+
+# The menu should include the following options:
+
+# Add Contact
+# View Contact
+# Edit Contact
+# Delete Contact
+# List All Contacts
+# Exit
+
+# This function will be the starting point of the program, helping
+# users navigate through the options.
+
+def display_menu():
+    print('Contact Book Menu:')
+    print('1. Add Concat')
+    print('2. View Contact')
+    print('3. Edit Contact')
+    print('4. Delete Contact')
+    print('5. List All Contacts')
+    print('6. Exit')
+
+
+display_menu()
